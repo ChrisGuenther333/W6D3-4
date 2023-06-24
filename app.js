7075f864fa86d096f370c0a2d74142a274075fe4
ChrisGuenther333 <CHRIS.GUENTHER4328@STU.MTEC.EDU>
2023-06-23 23:47:24 -0600

    Added overflow, able to delete/edit comments

--- app.js
+++ app.js
@@ -10,17 +10,76 @@ function checkFields() {
     } 
 }
 
-
-
 $('#submitButton').on('click', function() {
 
     if(checkFields() === 'checked') {
         $('.comments').prepend(`<div class="comment">
-            <p class="commentUser">` + $('#displayName').val() + `</p>
-            <p class="commentText">` + $('#commentText').val() + `</p>
+            <img src="./Images/facebook-profile-blank-face.jpeg" alt="Profile picture"></img>
+            <div class="userAndComment">
+                <p class="commentUser">` + $('#displayName').val() + `</p>
+                <p class="commentText">` + $('#commentText').val() + `</p>
+            </div>
+            <div class="editAndDelete">
+                <input class="editButton" type="button" value="Edit">
+                <input class="deleteButton" type="button" value="Delete">
+            </div>
         </div>`);
+
+        $('#displayName').val('');
+        $('#commentText').val('');
     }
     else{
         return;
     }
+});
+
+$('.comments').on('click', '.editButton', function() {
+    const comBox = $(this).parents()[1];
+    const cBChildren = $(comBox).children()[1];
+    const comText = $(cBChildren).children()[1];
+
+    $(this).toggleClass('editVisible')
+
+    if ($(this).hasClass('editVisible')) { 
+    $(comBox).append(`
+    <div class="editComment">
+        <input class="eCText" type="text" value="` + $(comText).text() + `">
+        <input class="eCButton" type="button" value="submit">
+    </div>`)
+    }
+    else {
+        $('.editComment').css('display', 'none')
+        $('.editComment').remove()
+    }
+
+});
+
+$('.comments').on('click', '.deleteButton', function() {
+    const thisParent = $(this).parents()[1];
+    $(thisParent).remove();
+});
+
+$('.comments').on('click', '.eCButton', function() {
+    const comBox = $(this).parents()[1];
+    const cBChildren = $(comBox).children()[1];
+    const comText = $(cBChildren).children()[1];
+
+    const eCBox = $(this).parents()[1];
+    const eCBChildren = $(eCBox).children()[3];
+    const eComText = $(eCBChildren).children()[0];
+    
+    $(comText).text($(eComText).val());
+
+    const backToEAD = $(comBox).children()[2];
+    const backToE = $(backToEAD).children()[0];
+
+    $(backToE).toggleClass('editVisible');
+    $(eCBChildren).css('display', 'none')
+    $(eCBChildren).remove()
+
+
+
+
+
+
 });
